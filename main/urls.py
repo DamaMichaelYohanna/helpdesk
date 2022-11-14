1a93ffc034df6ea0eba059a9f5ef3a35b8391c45
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-14 09:03:22 +0100

    update views and urls

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from main import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("service", views.service, name="service"),
+    path("faq", views.faq, name="faq"),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """view for home page"""
+    return render(request, 'home.html')
+
+
+def about(request):
+    """view for about us page"""
+    return render(request, 'about.hmtl')
+
+
+def contact(request):
+    """ view for contact us page"""
+    return render(request, 'contact.hmtl')
+
+
+def service(request):
+    """view for service page"""
+    return render(request, 'service.hmtl')
+
+
+def faq(request):
+    """view for frequently asked questions page"""
+    return render(request, 'faq.html')
