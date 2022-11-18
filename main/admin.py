7e5bb22f486fbd2bca59989e4e7595bedb88a439
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-18 08:47:22 +0100

    model for issues tracking. added same to admin site. also generated migrations file

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Issue
+admin.site.register(Issue)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Issue(models.Model):
+    """model for issue entry"""
+    ref = models.CharField(max_length=10)
+    complainant = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    complain = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'{self.ref}'
